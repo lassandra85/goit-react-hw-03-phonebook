d3aa36344f26b628d74632509ff8e82a75ba16a8
lassandra85 <105219220+lassandra85@users.noreply.github.com>
2023-03-08 21:37:03 +0100

    Initial commit

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 40px auto;
+  padding: 20px;
+  max-width: 650px;
+  text-align: center;
+  box-shadow: var(--main-shadow);
+  border-radius: 10px;
+  background-color: #ede3ec;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+  padding: 5px;
+  font-size: 44px;
+  color: var(--primary);
+  text-shadow: var(--main-shadow);
+  box-shadow: var(--main-shadow);
+  border-radius: 10px;
+  background-color: #f3badd;
+`;
+
+export const Subtitle = styled.h2`
+  margin-bottom: 20px;
+  padding: 5px;
+  font-size: 32px;
+  color: var(--primary);
+  text-shadow: var(--main-shadow);
+  box-shadow: var(--main-shadow);
+  border-radius: 10px;
+  background-color: #f3badd;
+`;
